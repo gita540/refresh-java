5a8cae2ac020f6c61e5e3c88f7de36c3c6ae11ce
Gita Mittala <gitasri.reddy@gmail.com>
2022-02-06 20:15:35 -0800

    array left rotation

---
+++ src/random_problems/ArrayRotation.java
@@ -0,0 +1,40 @@
+package random_problems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayRotation {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter input array length ");
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+
+    System.out.println("Enter array elements");
+    for (int i = 0; i < n; i++) {
+      nums[i] = sc.nextInt();
+    }
+    System.out.println("How many positions to rotate array");
+    int positions = sc.nextInt();
+    System.out.println("Input Array rotation");
+    System.out.println(Arrays.toString(nums));
+    ArrayRotation obj = new ArrayRotation();
+    int[] arr = obj.left_rotation(nums, positions);
+    System.out.println("Input Array After Left Rotation By " + positions + " Positions :");
+    System.out.println(Arrays.toString(arr));
+  }
+
+  //Time complexity :O(n^2)
+  public int[] left_rotation(int[] nums, int positions) {
+    for (int i = 0; i < positions; i++) {
+      int temp = nums[0];
+      for (int j = 0; j < nums.length - 1; j++) {
+        nums[j] = nums[j + 1];
+      }
+      nums[nums.length - 1] = temp;
+    }
+    return nums;
+  }
+
+}
