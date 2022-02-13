959d13cee485c15a9c65199584fe21f48a81fbde
Gita Mittala <gitasri.reddy@gmail.com>
2022-02-12 22:46:28 -0800

    add tests

--- src/random_problems/ProductExpectSelf.java
+++ src/random_problems/ProductExpectSelf.java
@@ -1,4 +1,4 @@
-package problems;
+package random_problems;
 
 import java.util.Arrays;
 import java.util.Scanner;
--- src/test/DecomposeStringTest.java
+++ src/test/DecomposeStringTest.java
@@ -1,4 +1,37 @@
+package test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import random_problems.DecomposeString;
+
 class DecomposeStringTest {
-  
+  private DecomposeString decomposeString;
+
+  @BeforeEach
+  void setUp() {
+    decomposeString = new DecomposeString();
+  }
+
+  @Test
+  void setDecomposeString_test1(){
+    Assertions.assertTrue(decomposeString.decomposeString("11100111"));
+  }
+
+  @Test
+  void setDecomposeString_test2(){
+    Assertions.assertFalse(decomposeString.decomposeString("111001111"));
+  }
+
+  @Test
+  void setDecomposeString_test3(){
+    Assertions.assertFalse(decomposeString.decomposeString("11"));
+  }
+
+  @Test
+  void setDecomposeString_test4(){
+    Assertions.assertFalse(decomposeString.decomposeString("101"));
+  }
 }
